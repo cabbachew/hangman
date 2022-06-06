d0af6b3c948f5eac88cc30d37f947cf86ddb92ff
Daniel Oh <cabbachew@gmail.com>
2022-06-06 15:30:53 +0900

    Reduce remaining guess count after guess

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -8,11 +8,11 @@ end
 
 class Hangman
   def initialize(word_pool)
-    @answer = word_pool.sample
-    @ans_array = answer.split("")
+    # Generate and store answer in array
+    @answer = word_pool.sample.split("") 
     @remaining_guesses = answer.length
   end
-  attr_reader :answer, :ans_array, :remaining_guesses
+  attr_reader :answer, :remaining_guesses
 
 # # Store answer in an array
 # ans_array = answer.split("")
@@ -24,20 +24,23 @@ class Hangman
 # remaining_guesses = ans_array.length
 
   def play
-    print_remaining_guesses(remaining_guesses)
+    guess = get_player_guess
+    @remaining_guesses -= 1
+    puts "You guessed: #{guess}"
+
+    print_remaining_guesses(@remaining_guesses)
+
+    puts "The answer is: #{answer}"
   end
 
   def print_remaining_guesses(remaining_guesses)
     puts "Guesses remaining: #{remaining_guesses}"
   end
 
-# print "Pick a letter: "
-
-# guess = gets.chomp.downcase
-
-# puts "The answer is: #{answer}"
-# puts "You guessed: #{guess}"
-
+  def get_player_guess
+    print "Pick a letter: "
+    return gets.chomp.downcase
+  end
 end
 
 Hangman.new(word_pool).play
