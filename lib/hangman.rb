7ea4b011a7a15ac12a525ebdf6f05440abae7e7b
Daniel Oh <cabbachew@gmail.com>
2022-06-06 12:57:16 +0900

    Filter word list for valid word lengths

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,1 +1,9 @@
 puts "Hangman initialized."
+
+all_words = File.readlines('google-10000-english-no-swears.txt').map(&:chomp)
+
+word_pool = all_words.select do |word|
+  word.length >= 5 && word.length <= 12
+end
+
+puts word_pool
